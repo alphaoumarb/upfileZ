32a41cbf1ae7696433dd729cfaf362ee68d22b74
Draker21 <drak3r21@hotmail.com>
2018-06-29 13:38:32 +0200

    test/erase

--- app/model/transfer.php
+++ app/model/transfer.php
@@ -9,21 +9,14 @@ Class Transfer extends Model{
             $error = 0;
             $emailExpediteur = $_POST['emailExpediteur'];
             $emailDestinataire = $_POST['emailDestinataire'];
-            print_r($emailDestinataire);
             print_r($emailExpediteur);
+            print_r($emailDestinataire);
 
             //Vérif si checkbox est true ou false
             $checkbox = isset($_POST['emailCopie']) ? 1 : 0;
 
             //Verif d'erreur lors d'upload
             if(!empty($_FILES)){
-
-                $ext = substr($_FILES['fileUpload']['name'], strrpos($_FILES['fileUpload']['name'], '.') + 1);
-                $nameGenerated = substr(md5($_FILES['fileUpload']['name']), 0, 5).microtime().'.'.$ext;
-
-                $path = ROOT . '\app\assets\file_uploaded/'. $nameGenerated;
-                $path = $path . basename( $_FILES['fileUpload']['name']);
-
                 if(isset($_FILES['fileUpload']['error'])){
                     switch($_FILES['fileUpload']['error']){ //ref : http://php.net/manual/fr/features.file-upload.errors.php
                         case 1:
@@ -48,46 +41,30 @@ Class Transfer extends Model{
                             break;
                     }
                     if($error == 0){
-                        print_r($_FILES['fileUpload']);
+                        $ext = substr($_FILES['fileUpload']['name'], strrpos($_FILES['fileUpload']['name'], '.') + 1);
+                        $nameGenerated = substr(md5($_FILES['fileUpload']['name']), 0, 5).microtime().'.'.$ext;
+        
+                        $path = ROOT . '\app\assets\file_uploaded/'. $nameGenerated;
+                        $path = $path . basename( $_FILES['fileUpload']['name']);
                         echo "<p>Votre fichier a bien été uploadé !</p>";
                     } else {
-                        return false;
+
                         echo $msg['msg'];
                     }
                 }
             } else {
-                return false;
                 echo "<p>Veuillez sélectionner un fichier</p>";
             }
-            
+            print_r($emailExpediteur);
             
             //Vérif si l'email Expéditeur est bon
-            $pattern = "/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD";
-            if(isset($emailExpediteur)){
-                $emailExpediteur = htmlspecialchars($emailExpediteur);
-                if(preg_match($pattern, $emailExpediteur)){
-                    echo 'Mail ok';//requete pour envoyer mail et enregistrer mail dans bdd//
-                } else {
-                    return false;
+            if(empty($emailExpediteur)){
                     echo "<p>L'email de votre destinaire n'est pas valide</p>";
-                }
-            }else{
-                return false;
-                echo "<p>Votre email n'est pas valide</p>";
             }
     
             //Vérif si l'email Destinaire est bon
-            if(isset($emailDestinataire)){
-                $emailDestinataire = htmlspecialchars($emailDestinataire);
-                if(preg_match($pattern, $emailDestinataire)){
-                    echo 'Mail ok';//requete pour envoyer mail et enregistrer mail dans bdd//
-                }else{
-                    return false;
-                    echo "<p>L'email de votre destinaire n'est pas valide</p>";
-                }
-            } else {
-                return false;
-                echo "<p>Veuillez entrer le mail de votre destinataire";
+            if(empty($emailDestinataire)){
+                    echo "<p>Veuillez entrer le mail de votre destinataire";
             }
 
             $db = Database::getInstance();
