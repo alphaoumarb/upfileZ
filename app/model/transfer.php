24261adfd69c1ae3b5d99ce62b21699ad8cd3021
Draker21 <drak3r21@hotmail.com>
2018-06-29 10:50:26 +0200

    update back-end

--- app/model/transfer.php
+++ app/model/transfer.php
@@ -14,6 +14,7 @@ Class Transfer extends Model{
 
             //Vérif si checkbox est true ou false
             $checkbox = isset($_POST['emailCopie']) ? 1 : 0;
+
             //Verif d'erreur lors d'upload
             if(!empty($_FILES)){
 
@@ -47,44 +48,54 @@ Class Transfer extends Model{
                             break;
                     }
                     if($error == 0){
-                        echo "Votre fichier a bien été uploadé !";
+                        print_r($_FILES['fileUpload']);
+                        echo "<p>Votre fichier a bien été uploadé !</p>";
                     } else {
+                        return false;
                         echo $msg['msg'];
                     }
-            
                 }
+            } else {
+                return false;
+                echo "<p>Veuillez sélectionner un fichier</p>";
+            }
             
             
-                //Vérif si l'email Expéditeur est bon
-                $pattern = "/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD";
-                if(isset($emailExpediteur)){
-                    $emailExpediteur = htmlspecialchars($emailExpediteur);
-                    if(preg_match($pattern, $emailExpediteur)){
-                        echo 'Mail ok';//requete pour envoyer mail et enregistrer mail dans bdd//
-                    }else{
-                        echo "Votre email n'est pas valide";
-                    }
+            //Vérif si l'email Expéditeur est bon
+            $pattern = "/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD";
+            if(isset($emailExpediteur)){
+                $emailExpediteur = htmlspecialchars($emailExpediteur);
+                if(preg_match($pattern, $emailExpediteur)){
+                    echo 'Mail ok';//requete pour envoyer mail et enregistrer mail dans bdd//
+                } else {
+                    return false;
+                    echo "<p>L'email de votre destinaire n'est pas valide</p>";
                 }
+            }else{
+                return false;
+                echo "<p>Votre email n'est pas valide</p>";
+            }
     
-                //Vérif si l'email Destinaire est bon
-                if(isset($emailDestinataire)){
-                    $emailDestinataire = htmlspecialchars($emailDestinataire);
-                    if(preg_match($pattern, $emailDestinataire)){
-                        echo 'Mail ok';//requete pour envoyer mail et enregistrer mail dans bdd//
-                    }else{
-                        echo "L'email de votre destinaire n'est pas valide";
-                    }
+            //Vérif si l'email Destinaire est bon
+            if(isset($emailDestinataire)){
+                $emailDestinataire = htmlspecialchars($emailDestinataire);
+                if(preg_match($pattern, $emailDestinataire)){
+                    echo 'Mail ok';//requete pour envoyer mail et enregistrer mail dans bdd//
+                }else{
+                    return false;
+                    echo "<p>L'email de votre destinaire n'est pas valide</p>";
                 }
+            } else {
+                return false;
+                echo "<p>Veuillez entrer le mail de votre destinataire";
+            }
 
-                $db = Database::getInstance();
-                $sql = 'INSERT INTO `transfer`(email_expediteur, email_destinataire, email_copie) VALUES ('. $emailExpediteur . ',' . $emailDestinataire . ',' . $checkbox . ')';
-                $stmt = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-                return $stmt;
-            }         
-        } else {
-            return false;
-            echo "Veuillez remplir tout les champs correctement";
-        }
+            $db = Database::getInstance();
+            $sql = 'INSERT INTO `transfer`(email_expediteur, email_destinataire, email_copie) VALUES ('. $emailExpediteur . ',' . $emailDestinataire . ',' . $checkbox . ')';
+            $stmt = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+            return $stmt;
+           
+        } 
       
     }  
 }
